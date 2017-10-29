b15185941ca4f8d6e75abab278d4681e57018f34
Felipe <fbarrios.ing.informatico@gmail.com>
2017-10-29 15:37:49 -0300

    ADD: routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root to: 'users#index'
   get "/users", to: 'users#index', as: :user
   post '/users', to: 'users#create'
   get '/users/new', to: 'users#new', as: :new_user
